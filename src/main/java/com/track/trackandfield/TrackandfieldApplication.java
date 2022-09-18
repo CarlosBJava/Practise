d3170c998fa7248f398e42f1c87c2b393b22a876
CarlosBJava <carlosbena@live.nl>
2022-09-18 15:47:51 +0200

    Initial commit

---
+++ src/main/java/com/track/trackandfield/TrackandfieldApplication.java
@@ -0,0 +1,13 @@
+package com.track.trackandfield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackandfieldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackandfieldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/track/trackandfield/TrackandfieldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.track.trackandfield;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackandfieldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
